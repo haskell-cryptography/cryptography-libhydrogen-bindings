c7f558a6e1ec2d3258f4c569ae9d4ee30d5915fe
Koz Ross <koz.ross@retro-freedom.nz>
2022-03-27 13:51:54 +1300

    CI

---
+++ .github/check.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+ormolu_bin=$1
+
+cabal_fmt_bin=$2
+
+find src -name "*.hs" | while read -r f; do
+if ! "${ormolu_bin}" -m 'check' "$f" 1> /dev/null 2> /dev/null ; then
+  echo "$f is not formatted, aborting."
+  exit 1
+fi
+done
+
+find . -maxdepth 1 -name "*.cabal" | while read -r f; do
+if ! "${cabal_fmt_bin}" -c "$f" ; then
+  echo "$f is not formatted, aborting."
+  exit 1
+fi
+done
